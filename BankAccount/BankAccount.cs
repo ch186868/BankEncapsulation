27c090a35c40cf92562fe78f2f60c39b895f6f9a
Chris Hornbuckle <ch186868@gmail.com>
2021-02-02 15:02:07 -0600

    final

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        private double _balance;
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount,0:c} to your account.");
+            _balance = amount;
+        }
+
+        public double GetBalance()
+
+        {
+            return _balance;
+        }
+       
+
+        
+    }
+}
